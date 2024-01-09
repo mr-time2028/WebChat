49c58640029acf8b20b44b9fecc1d6316db48282
mr-time2028 <hamidm.ap00@gmail.com>
2024-01-09 18:29:51 +0330

    Add dummy logic to handle ws connection in the client side

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     });
 });
 
+let socket = null
+let messageField = document.getElementById("message_input")
+let outputField = document.getElementById("message_output")
+sendMsgBtn = document.getElementById("send_message")
 
+document.addEventListener("DOMContentLoaded", function() {
+    socket = new ReconnectingWebSocket("ws://127.0.0.1:8000/ws", null, {debug: true, reconnectInterval: 3000})
+    console.log("connected to the weboscket")
+
+    socket.onmessage = msg => {
+        data = JSON.parse(msg.data)
+        console.log(data.action)
+    }
+})
